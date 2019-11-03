ea54f9cf53d3729e579f5eae3048663ed143daaa
AJay Marathe <0107amar@gmail.com>
2019-11-03 10:59:12 +0530

    working on slateblueToggle

---
+++ src/components/slateblueToggle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="custom-control custom-switch">
+    <input
+      type="checkbox"
+      v-bind:checked="checked"
+      v-on:change="$emit('toggle', $event.target.checked)"
+      class="custom-control-input"
+    />
+    <label class="custom-control-label"></label>
+    <!-- <label class="custom-control-label">Toggle this switch element</label> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "slateblueToggle",
+  model: {
+    prop: "checked",
+    event: "toggle"
+  },
+  props: {
+    checked: Boolean
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
