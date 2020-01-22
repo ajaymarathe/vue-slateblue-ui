2b2645b4af6a0aeade4a2399e0811dbe659e6b9c
Ajay Marathe <ajay.marathe@xebecindia.com>
2020-01-22 10:52:07 +0530

    recomiled all files

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
-// module.exports = {
-//     chainWebpack: config => {
-//         config.plugin('optimize-css').tap(([options]) => {
-//             options.cssnanoOptions.preset[1].svgo = false
-//             return [options]
-//         })
-//     }
-// }
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/slateblue-ui/'
+    : '/',
+    // chainWebpack: config => {
+    //     config.plugin('optimize-css').tap(([options]) => {
+    //         options.cssnanoOptions.preset[1].svgo = false
+    //         return [options]
+    //     })
+    // },
+}
